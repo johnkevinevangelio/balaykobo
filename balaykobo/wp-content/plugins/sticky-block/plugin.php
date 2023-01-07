cd9a20e7d21b84130ee2c45ff95bb497fa82155a
John Kevin Evangelio <johnkevinevangelio@maroonstudios.com>
2023-01-08 02:20:48 +0800

    feat: initial commit

---
+++ balaykobo/wp-content/plugins/sticky-block/plugin.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Sticky Block
+Description: Adds a Sticky Block to the Gutenberg Block editor.
+Author: Senff
+Author URI: http://www.senff.com
+Version: 1.11
+License: GPL2+
+License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
+
+
